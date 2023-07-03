5101b9189ed65af0af1c27b353990a64c9bacc66
necket <nik.sherbakov.01@gmail.com>
2023-07-03 18:46:01 +0300

    add polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
@@ -15,6 +16,8 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
